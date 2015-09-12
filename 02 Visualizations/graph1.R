f7f58f1bab0ef9f3255ce32f03476a8dc661aaa9
ibrahimnagib <ibrahim.nagib@utexas.edu>
2015-09-12 11:46:44 -0500

    updating visualizations

--- 02 Visualizations/graph1.R
+++ 02 Visualizations/graph1.R
@@ -1,10 +1,6 @@
-require("jsonlite")
-require("RCurl")
-# Change the USER and PASS below to be your UTEid
-df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_in2422', PASS='orcl_in2422', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+#Graph 1
 
-# 
-require(extrafont)
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_in2422', PASS='orcl_in2422', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
 
 ggplot() + 
   coord_cartesian() + 
--- 02 Visualizations/graph4.R
+++ 02 Visualizations/graph4.R
@@ -1,12 +1,9 @@
-require("jsonlite")
-require("RCurl")
-# Change the USER and PASS below to be your UTEid
+# Graph 4
+
 df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from titanic where sex is not null "'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_in2422', PASS='orcl_in2422', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
-df
-summary(df)
-head(df)
+
 # 
-require(extrafont)
+
 ggplot() + 
   coord_cartesian() + 
   #scale_x_continuous() +
